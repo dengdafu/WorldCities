cac9843153d68b723868e7262b7438e08769e275
dengdafu <dengdafu1992@hotmail.com>
2021-11-21 18:33:11 -0500

    Add CountryName column for Cities. Code up to page 385.

--- ClientApp/src/app/cities/cities.component.ts
+++ ClientApp/src/app/cities/cities.component.ts
@@ -14,7 +14,7 @@ import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
   styleUrls: ['./cities.component.css']
 })
 export class CitiesComponent implements OnInit {
-  public displayedColumns: string[] = ['id', 'name', 'lat', 'lon'];
+  public displayedColumns: string[] = ['id', 'name', 'lat', 'lon', 'countryName'];
   public cities: MatTableDataSource<City>;
 
   defaultPageIndex: number = 0;

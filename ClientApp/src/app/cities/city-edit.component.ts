61af4a540a51f7efa52acf8d52b797cf0c96bd81
dengdafu <dengdafu1992@hotmail.com>
2021-11-28 10:24:24 -0500

    Add Logger - Serilog. Code up to page 458. Chapter 8 done.

--- ClientApp/src/app/cities/city-edit.component.ts
+++ ClientApp/src/app/cities/city-edit.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { FormControl, FormGroup, Validators, AbstractControl, AsyncValidatorFn } from '@angular/forms';
+import { Component, Inject, OnInit, OnDestroy } from '@angular/core';
+// import { HttpClient, HttpParams } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
-
-import { City } from './city';
-import { Country } from './../countries/country';
+import { FormGroup, FormControl, Validators, AbstractControl, AsyncValidatorFn } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { BaseFormComponent } from '../base.form.component';
+
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+import { City } from './city';
+import { Country } from '../countries/country';
 import { CityService } from './city.service';
 import { ApiResult } from '../base.service';
 
@@ -15,7 +19,8 @@ import { ApiResult } from '../base.service';
   templateUrl: './city-edit.component.html',
   styleUrls: ['./city-edit.component.css']
 })
-export class CityEditComponent extends BaseFormComponent implements OnInit {
+export class CityEditComponent
+  extends BaseFormComponent implements OnInit, OnDestroy {
 
   // the view title
   title: string;
@@ -23,49 +28,98 @@ export class CityEditComponent extends BaseFormComponent implements OnInit {
   // the form model
   form: FormGroup;
 
-  // the city object to edit
+  // the city object to edit or create
   city: City;
 
   // the city object id, as fetched from the active route:
-  // It's NULL when we are adding a new City,
-  // and not NULL when we are editing an existing one.
+  // It's NULL when we're adding a new city,
+  // and not NULL when we're editing an existing one.
   id?: number;
 
-  // the countries array for the select
-  countries: Country[];
+  // the countries observable for the select (using async pipe)
+  countries: Observable<ApiResult<Country>>;
+
+  // Activity Log (for debugging purposes)
+  activityLog: string = '';
+
+  // Notifier subject (to avoid memory leaks)
+  private destroySubject: Subject<boolean> = new Subject<boolean>();
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private router: Router,
-    private cityService: CityService
-  ) {
+    private cityService: CityService) {
     super();
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.form = new FormGroup({
       name: new FormControl('', Validators.required),
-      lat: new FormControl('', [Validators.required, Validators.pattern(/^[-]?[0-9]+(\.[0-9]{1,4})?$/)]),
-      lon: new FormControl('', [Validators.required, Validators.pattern(/^[-]?[0-9]+(\.[0-9]{1,4})?$/)]),
+      lat: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^[-]?[0-9]+(\.[0-9]{1,4})?$/)
+      ]),
+      lon: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^[-]?[0-9]+(\.[0-9]{1,4})?$/)
+      ]),
       countryId: new FormControl('', Validators.required)
     }, null, this.isDupeCity());
+
+    // react to form changes
+    this.form.valueChanges
+      .pipe(takeUntil(this.destroySubject))
+      .subscribe(() => {
+        if (!this.form.dirty) {
+          this.log("Form Model has been loaded.");
+        }
+        else {
+          this.log("Form was updated by the user.");
+        }
+      });
+
+    // react to changes in the form.name control
+    this.form.get("name")!.valueChanges
+      .pipe(takeUntil(this.destroySubject))
+      .subscribe(() => {
+        if (!this.form.dirty) {
+          this.log("Name has been loaded with initial values.");
+        }
+        else {
+          this.log("Name was updated by the user.");
+        }
+      });
 
     this.loadData();
   }
 
+  ngOnDestroy() {
+    // emit a value with the takeUntil notifier
+    this.destroySubject.next(true);
+    // unsubscribe from the notifier itself
+    this.destroySubject.unsubscribe();
+  }
+
+  log(str: string) {
+    this.activityLog += "["
+      + new Date().toLocaleString()
+      + "] " + str + "<br />";
+  }
+
   loadData() {
+
+    // load countries
     this.loadCountries();
 
-    // retrieve the ID from the 'id' parameter
+    // retrieve the ID from the 'id'
     this.id = +this.activatedRoute.snapshot.paramMap.get('id');
-
     if (this.id) {
       // EDIT MODE
 
       // fetch the city from the server
       this.cityService.get<City>(this.id).subscribe(result => {
         this.city = result;
-        this.title = 'Edit - ' + this.city.name;
+        this.title = "Edit - " + this.city.name;
 
         // update the form with the city value
         this.form.patchValue(this.city);
@@ -74,50 +128,55 @@ export class CityEditComponent extends BaseFormComponent implements OnInit {
     else {
       // ADD NEW MODE
 
-      this.title = 'Create a new City';
+      this.title = "Create a new City";
     }
   }
 
   loadCountries() {
     // fetch all the countries from the server
-    this.cityService.getCountries<ApiResult<Country>>(
-      0,
-      9999,
-      'name',
-      null,
-      null,
-      null
-    ).subscribe(result => {
-      this.countries = result.data;
-    }, error => console.error(error));
+    this.countries = this.cityService
+      .getCountries<ApiResult<Country>>(
+        0,
+        9999,
+        "name",
+        null,
+        null,
+        null,
+      );
   }
 
   onSubmit() {
+
     var city = (this.id) ? this.city : <City>{};
 
-    city.name = this.form.get('name').value;
-    city.lat = +this.form.get('lat').value;
-    city.lon = +this.form.get('lon').value;
+    city.name = this.form.get("name").value;
+    city.lat = +this.form.get("lat").value;
+    city.lon = +this.form.get("lon").value;
     city.countryId = +this.form.get("countryId").value;
 
     if (this.id) {
       // EDIT mode
+      this.cityService
+        .put<City>(city)
+        .subscribe(result => {
 
-      this.cityService.put<City>(city).subscribe(result => {
-        console.log('City ' + city.id + ' has been updated.');
+          console.log("City " + city.id + " has been updated.");
 
-        // go back to cities view
-        this.router.navigate(['/cities']);
-      }, error => console.error(error));
+          // go back to cities view
+          this.router.navigate(['/cities']);
+        }, error => console.error(error));
     }
     else {
       // ADD NEW mode
-      this.cityService.post<City>(city).subscribe(result => {
-        console.log('City ' + result.id + ' has been created.');
+      this.cityService
+        .post<City>(city)
+        .subscribe(result => {
 
-        // go back to cities view
-        this.router.navigate(['/cities']);
-      }, error => console.error(error));
+          console.log("City " + result.id + " has been created.");
+
+          // go back to cities view
+          this.router.navigate(['/cities']);
+        }, error => console.error(error));
     }
   }
 
@@ -130,9 +189,10 @@ export class CityEditComponent extends BaseFormComponent implements OnInit {
       city.lon = +this.form.get("lon").value;
       city.countryId = +this.form.get("countryId").value;
 
-      return this.cityService.isDupeCity(city).pipe(map(result => {
-        return (result ? { isDupeCity: true } : null);
-      }));
+      return this.cityService.isDupeCity(city)
+        .pipe(map(result => {
+          return (result ? { isDupeCity: true } : null);
+        }));
     }
   }
 }
--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 using WorldCities.Data;
 
 namespace WorldCities
@@ -88,6 +89,9 @@ namespace WorldCities
                     spa.UseAngularCliServer(npmScript: "start");
                 }
             });
+
+            // Use the SeriLog request Logging middleware to Log HTTP requests.
+            app.UseSerilogRequestLogging();
         }
     }
 }
